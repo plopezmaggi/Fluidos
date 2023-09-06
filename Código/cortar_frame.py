789f7af6507f76a54004f8992bcbbfbedbe453a8
Florencia Cammarata <florencia_magali@hotmail.com.ar>
2023-09-06 18:03:15 -0300

    Agrego script para cortar un frame.

---
+++ Código/cortar_frame.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -- coding: utf-8 --
+"""
+Created on Wed Sep  6 17:50:22 2023
+
+@author: plopezmaggi
+
+Corta un frame de un video. Asume que el video está en la carpeta que Spyder tiene abierta, y guarda ahí la imagen.
+"""
+
+import cv2
+
+video = ""
+
+video = cv2.VideoCapture(video)
+
+for i in range (100):
+    frame, = video.read()
+
+cv2.imsave("cuadro.tif", frame)
