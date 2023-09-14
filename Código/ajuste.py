5aa95cb9f3d30880cda3a5001d875e1b63f4e4ab
Pilar López Maggi <pilar.lopezmaggi@gmail.com>
2023-09-14 15:58:35 -0300

    Modifico código de análisis.

--- Código/ajuste.py
+++ Código/ajuste.py
@@ -9,9 +9,8 @@ Created on Wed Sep  6 17:29:28 2023
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-# %matplotlib nbagg
 import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
+from matplotlib import colormaps as cm
 from matplotlib import rcParams  # Para aumentar la resolución de los gráficos de Matplot
 import seaborn as sns
 from matplotlib.colors import LogNorm
@@ -24,116 +23,134 @@ rcParams['figure.dpi'] = 100
 plt.style.use('seaborn-v0_8-poster')
 
 #%%
+
+def calcularCentro(datos, porcentaje=1.0, GRAFICAR=False):
+    # Tirar velocidades nulas
+    datos = np.array([dato for dato in datos if dato[2]**2 + dato[3]**2 > 0])
+    
+    # Cantidad de puntos para usar
+    n = int(len(datos) * porcentaje)
+    
+    # Ordenar según el módulo de la velocidad y quedarnos con las más altas
+    ordenados = np.array(sorted(datos, key=lambda row : row[2]**2 + row[3]**2, reverse=True))[:n]
+    
+    x, y, u, v, err_u, err_v = ordenados.T
+    
+    # Pasar a polares
+    # El campo de velocidades en cartesianas es (u, v)
+    theta = np.arctan(v / u) # ángulos que forma el vector velocidad con la horizontal
+
+    # Calculo la pendiente de la recta normal a la velocidad
+    m = np.tan(theta - np.pi / 2) # pendientes
+
+    # La muevo hasta la posición de la partícula
+    b = y - m * x # ordenadas al origen
+
+    # Listas para guardar las coordenadas (x, y) entre cada par de rectas
+    X_intercept = []
+    Y_intercept = []
+
+    # Calcular la intersección de cada par posible de rectas
+    for i in range(len(x)):
+        m1 = m[i]
+        b1 = b[i]
+        for j in range(i+1,len(x)):
+            m2 = m[j]
+            b2 = b[j]
+            x_intercept = (b2-b1)/(m1-m2)
+            y_intercept = x_intercept*m1 + b1
+            X_intercept.append(x_intercept)
+            Y_intercept.append(y_intercept)
+    
+    if GRAFICAR:
+        # Graficar todas las rectas y las intersecciones
+        fig, ax = plt.subplots()
+    
+        X_lin = np.linspace(np.min(x), np.max(x), 10) # vector de valores de x para graficar
+    
+        for i in range(len(x_sel)):
+            ax.plot(X_lin, m[i]*X_lin + b[i], c='k', lw=.5, alpha=.5)
+            
+        ax.scatter(X_intercept, Y_intercept, c='r', s=5, alpha=.4, zorder=5)
+        ax.set_ylim(np.min(y), np.max(y))
+        ax.set_xlim(np.min(x), np.max(x))
+        ax.quiver(x,y,u,v, color=C, edgecolor='k', linewidth=.3, zorder=3)
+        plt.show()
+
+    # Finalmente, buscamos el centro como el punto medio de todas las intersecciones, y lo graficamos:
+    x_centro = np.mean(X_intercept)
+    y_centro = np.mean(Y_intercept)
+
+    err_x_centro = np.std(X_intercept)
+    err_y_centro = np.std(Y_intercept)
+    
+    return (x_centro, y_centro), (err_x_centro, err_y_centro)
+
+def cargarDatos(video):
+    # Abrir posiciones
+    datosPosiciones = np.load(video + 'posiciones.npz')
+    
+    # Abrir campo de velocidades promediado (el que se guarda en calcularCampos.py)
+    datosVel = np.load(video + 'promedio-vel.npz')
+
+    x, y, u, v, err_u, err_v = datosPosiciones['x'], datosPosiciones['y'], datosVel['U'], datosVel['V'], datosVel['Uerr'], datosVel['Verr']
+    
+    datos = np.column_stack((x, y, u, v, err_u, err_v))
+    
+    return datos
+    
+def cmap(u, v, colormap):
+    color = np.hypot(u, v)
+    color = (color - min(color)) / (max(color) - min(color))
+    return cm[colormap](color)
+
+def graficoTangencial
+
+
+#%%
 # Videos
 velocidades = ['3-5', '4', '4-5']
 fluidos = [30, 37, 50]
 
 #%%
 
-
 ### Primero cargamos los datos y metemos las coordenadas y las velocidades en una lista.
 video = '37v3-5e/' # <----- ACÁ VA EL VIDEO PARA ANALIZAR
 
-# Abrir campo de velocidades promediado (el que se guarda en calcularCampos.py)
-datosPosiciones = np.load(video + 'posiciones.npz')
-datosVel = np.load(video + 'promedio-vel.npz')
+datos = cargarDatos(video)
 
-x, y, u, v, err_u, err_v = datosPosiciones['x'], datosPosiciones['y'], datosVel['U'], datosVel['V'], datosVel['Uerr'], datosVel['Verr']
-
+centroTot, c = calcularCentro(datos)
+centroFiltrado, c = calcularCentro(datos, 0.05)
 #%%
 
-# Gráfico del campo de velocidades
-color = np.hypot(u, v)
-color = (color - min(color)) / (max(color) - min(color))
-C = plt.cm.plasma(color)
-
-#%%
-
-fig, ax = plt.subplots(1,1, figsize=(8,8))
-Q = ax.quiver(x,y,u,v, color=C)
-
-# Seleccionamos los puntos que utilizaremos para calcular el centro (filtramos con un mínimo de velocidad)
-v_threshold = 0 #<---- MIRAR QUÉ CONVIENE PONER ACÁ
-
-idx = np.array([i for i in range(len(x)) if (u[i]**2 + v[i]**2) > v_threshold**2])
-
-if len(idx) > 0:
-    x_sel = x[idx]
-    y_sel = y[idx]
-    u_sel = u[idx]
-    v_sel = v[idx]
-
-else:
-    x_sel, y_sel, u_sel, v_sel = np.array([]), np.array([]), np.array([]), np.array([])
-
-ax.scatter(x_sel, y_sel, c='r', s=5)
-
-#%%
-
-"""
-Para calcular el centro, se grafican para un conjunto de puntos las rectas perpendiculares a sus velocidades,
-que se cruzarán en el centro del vórtice si las velocidades son tangenciales.
-Para esto se calcula la pendiente utilizando el ángulo theta formado entre la velocidad y la dirección horizontal,
-y la ordenada al origen a partir de pedir que la recta pase por el punto, de coordenadas (x,y).
-Luego se buscan los puntos de intersección entre todo par de rectas y se calcula el valor medio de todos ellos,
-donde se determina el centro de coordenadas.
-"""
-
-# Pasar a polares
-# El campo de velocidades en cartesianas es (u, v)
-theta = np.arctan(v_sel / u_sel) # ángulos que forma el vector velocidad con la horizontal
-
-# Calculo la pendiente de la recta normal a la velocidad
-m = np.tan(theta - np.pi / 2) # pendientes
-
-# La muevo hasta la posición de la partícula
-b = y_sel - m * x_sel # ordenadas al origen
-
-# Listas para guardar las coordenadas (x, y) entre cada par de rectas
-X_intercept = []
-Y_intercept = []
-
-# Calcular la intersección de cada par posible de rectas
-for i in range(len(x_sel)):
-    m1 = m[i]
-    b1 = b[i]
-    for j in range(i+1,len(x_sel)):
-        m2 = m[j]
-        b2 = b[j]
-        x_intercept = (b2-b1)/(m1-m2)
-        y_intercept = x_intercept*m1 + b1
-        X_intercept.append(x_intercept)
-        Y_intercept.append(y_intercept)
-
-# Graficar todas las rectas y las intersecciones
-fig, ax = plt.subplots()
-
-X_lin = np.linspace(np.min(x), np.max(x), 10) # vector de valores de x para graficar
-
-for i in range(len(x_sel)):
-    ax.plot(X_lin, m[i]*X_lin + b[i], c='k', lw=.5, alpha=.5)
+# Comparo todas las velocidades para un mismo fluido
+fluido = 30
+for vel in velocidades:
+    video = f"{fluido}v{vel}e/"
     
-ax.scatter(X_intercept, Y_intercept, c='r', s=5, alpha=.4, zorder=5)
-ax.set_ylim(np.min(y), np.max(y))
-ax.set_xlim(np.min(x), np.max(x))
-ax.quiver(x,y,u,v, color=C, edgecolor='k', linewidth=.3, zorder=3)
-plt.show()
+    if not os.path.isdir(video):
+        continue
+    
+#%%
+plt.close('all')
+plt.figure(figsize=(8,8))
+plt.imshow(plt.imread(video + 'cuadros/0544.jpg'), extent = [min(datos[:, 0]), max(datos[:, 0]), min(datos[:, 1]), max(datos[:, 1])])
+plt.quiver(datos[:, 0], datos[:, 1], datos[:, 2], datos[:, 3], color=C, alpha=0.7)
 
-# Finalmente, buscamos el centro como el punto medio de todas las intersecciones, y lo graficamos:
-x_centro = np.mean(X_intercept)
-y_centro = np.mean(Y_intercept)
+plt.scatter([x_v], [y_v], label = "Filtrados")
+# plt.scatter([x2], [y2], label="Sin filtrar")
+# plt.legend()
 
-err_x_centro = np.std(X_intercept)
-err_y_centro = np.std(Y_intercept)
 
 #%%
+C = cmap(datos[:, 2], datos[:, 3], 'plasma')
+
 fig, ax = plt.subplots(figsize=(8,8))
 ax.imshow(plt.imread(video + 'cuadros/0544.jpg'), extent = [min(x), max(x), min(y), max(y)])
 ax.quiver(x,y,u,v, color=C)
 # ax.scatter(x_centro, y_centro, c='r')
 plt.xlabel('Distancia [cm]')
 plt.ylabel('Distancia [cm]')
-#plt.savefig('campovel.png')
 plt.show()
 
 #%%
